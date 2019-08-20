52908dc5e84a334078d6c49987ecf01de4f9693b
jrivera6 <jhonny.riverap6@gmail.com>
2019-08-19 21:11:44 -0500

    First commit

---
+++ Caso3/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Caso3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgPedidos = new System.Windows.Forms.DataGridView();
+            this.dgProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de pedidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lista de productos";
+            // 
+            // dgPedidos
+            // 
+            this.dgPedidos.AllowUserToAddRows = false;
+            this.dgPedidos.AllowUserToDeleteRows = false;
+            this.dgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPedidos.Location = new System.Drawing.Point(80, 74);
+            this.dgPedidos.Name = "dgPedidos";
+            this.dgPedidos.ReadOnly = true;
+            this.dgPedidos.Size = new System.Drawing.Size(582, 150);
+            this.dgPedidos.TabIndex = 2;
+            this.dgPedidos.DoubleClick += new System.EventHandler(this.dgPedidos_DoubleClick);
+            // 
+            // dgProductos
+            // 
+            this.dgProductos.AllowUserToAddRows = false;
+            this.dgProductos.AllowUserToDeleteRows = false;
+            this.dgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProductos.Location = new System.Drawing.Point(80, 287);
+            this.dgProductos.Name = "dgProductos";
+            this.dgProductos.ReadOnly = true;
+            this.dgProductos.Size = new System.Drawing.Size(582, 156);
+            this.dgProductos.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 516);
+            this.Controls.Add(this.dgProductos);
+            this.Controls.Add(this.dgPedidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgPedidos;
+        private System.Windows.Forms.DataGridView dgProductos;
+    }
+}
+
---
+++ Caso3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caso3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString);
+
+        public void listaPedidos()
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_listapedidos_registrados", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    //da.SelectCommand.Parameters.AddWithValue("@anio", cbAñoPedido.SelectedValue);
+
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Pedidos");
+                        dgPedidos.DataSource = df.Tables["Pedidos"];
+                        //lblPedido.Text = df.Tables["Pedidos"].Rows.Count.ToString();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listaPedidos();
+        }
+
+        private void dgPedidos_DoubleClick(object sender, EventArgs e)
+        {
+            int codigo;
+            codigo = Convert.ToInt32(dgPedidos.CurrentRow.Cells[0].Value);
+            using (SqlCommand cmd = new SqlCommand("usp_detalle_pedido", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@idpedido", codigo);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "detallesdepedidos");
+                        dgProductos.DataSource = df.Tables["detallesdepedidos"];
+                        //lblDetalle.Text = df.Tables["detallesdepedidos"].Compute("Sum(monto)", "").ToString();
+
+                    }
+                }
+            }
+        }
+    }
+}
